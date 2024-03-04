af3ee4cbc43b7cdc3118f5b7d27af7208ce479f6
gcater1 <geoffcater123@gmail.com>
2024-03-04 16:52:49 -0500

    continuing to fix vercel err

--- proj1checkList/src/pages/index.tsx
+++ proj1checkList/src/pages/index.tsx
@@ -75,7 +75,7 @@ const Content: React.FC = () => {
   return (
     <>
       <input ref={inputRef} type="text" placeholder="Type a task..." className="border-2 border-gray-300 p-2 rounded-md" />
-      <button onClick={handleAddTask} className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button onClick={() =>handleAddTask} className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Add Task
       </button>
       <div>
