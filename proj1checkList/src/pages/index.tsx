436561280d10a555ee7b0dc6ac729e3c0383621f
gcater1 <geoffcater123@gmail.com>
2024-03-04 16:47:34 -0500

    commenting out lines to make vercel work

--- proj1checkList/src/pages/index.tsx
+++ proj1checkList/src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
+//import Link from "next/link";
 import React from "react";
 import { useRef } from "react";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.post.hello.useQuery({ text: "from tRPC" });
+  //const hello = api.post.hello.useQuery({ text: "from tRPC" });
 
   return (
     <div>
