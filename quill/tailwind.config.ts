7d4c38059f3b77c238c479eaba438980e52f6bf0
gcater1 <geoffcater123@gmail.com>
2024-03-10 19:03:40 -0400

    adding quill

---
+++ quill/tailwind.config.ts
@@ -0,0 +1,30 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens:{
+        '2x1': '1400px',
+      },
+    },
+    extend: {
+      maxWith: {
+        "8x1": "1408px",
+      },
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(-ring))",
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
