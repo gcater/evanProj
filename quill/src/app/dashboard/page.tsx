1ac1a7b42b6a3f119de0371df93906a856623efa
gcater1 <geoffcater123@gmail.com>
2024-03-12 13:13:01 -0400

    done with dashboard

--- quill/src/app/dashboard/page.tsx
+++ quill/src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import Dashboard from "@/components/Dashboard";
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -6,9 +8,17 @@ const Page = async () => {
     const {getUser} = getKindeServerSession()
     const user = await getUser()
 
-    if (!user|| !user.id) redirect('/auth-callback?origin=dashboard') 
+    if (!user|| !user.id) redirect('/auth-callback?origin=dashboard')
+    
+    const dbUser = await db.user.findFirst({
+        where: {
+            id: user.id
+        }
+    })
 
-    return <div>{user.email}</div>
+    if(!dbUser) redirect('/auth-callback?origin=dashboard')
+
+    return <Dashboard/>
 }
 
 export default Page;
